e90be52f4748b85013f5e83c5b06d80cbb0838f8
syahmi <wanahmadsyahmi26@gmail.com>
2021-08-18 21:15:40 +0800

    latest

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,8 +53,7 @@
                 <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5" type="submit">Log in</button>
             </form>
             <div class="text-center mt-5 text-lg fs-4">
-                <p class="text-gray-600">Don't have an account? <a href="{{ route('register') }}" class="font-bold">Sign
-                        up</a>.</p>
+                
                 @if (Route::has('password.request'))
                 <p><a class="font-bold" href="{{ route('password.request') }}">Forgot password?</a>.</p>
                 @endif
