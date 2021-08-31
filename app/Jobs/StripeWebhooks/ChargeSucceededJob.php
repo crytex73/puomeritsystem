c1ac2807c8dfea28baaccade057addf70ab027af
syahmi <wanahmadsyahmi26@gmail.com>
2021-08-31 18:10:23 +0800

    problem webhook

---
+++ app/Jobs/StripeWebhooks/ChargeSucceededJob.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs\StripeWebhooks;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Spatie\WebhookClient\Models\WebhookCall;
+use Illuminate\Support\Facades\Log;
+use App\Models\Compound;
+
+class ChargeSucceededJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $webhookCall;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(webhookCall $webhookCall)
+    {
+        $this->webhookCall = $webhookCall;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $SessionData = $this->webhookCall-payload['data']['object'];
+
+        $compound_id = intval($SessionData['metada']['data_key']);
+        $compound = Compound::find($compound_id);
+        $compound->payment_status = true;
+        $compound->save();
+    }
+}
