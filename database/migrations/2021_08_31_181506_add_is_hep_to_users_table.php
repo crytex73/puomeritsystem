2086a388d012758580f2e2e7263378d5d81f60e2
syahmi <wanahmadsyahmi26@gmail.com>
2021-09-01 02:18:49 +0800

    settle hep

--- database/migrations/2021_08_31_181506_add_is_hep_to_users_table.php
+++ database/migrations/2021_08_31_181506_add_is_hep_to_users_table.php
@@ -14,7 +14,7 @@ class AddIsHepToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_hep');
+            $table->boolean('is_hep')->default(false);
         });
     }
 
